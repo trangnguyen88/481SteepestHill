0879260e5bc7d226ac4e0472d665084779ad5eca
trangnguyen88 <trangnguyen88@users.noreply.github.com>
2017-06-15 07:47:30 -0700

    Add files via upload

---
+++ Location.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace steep_hill_climbing
+{
+    public class Location
+    {
+        public int x;
+        public int y;
+        public int currentMessyCount;
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace steep_hill_climbing
+{
+    class Program
+    {
+        public static void Main()
+        {
+            String[] lines = File.ReadAllLines("in.txt");
+            int[,] matrix = getMatrix(lines,0,2);
+            int[,] goalMatrix = getMatrix(lines,4,6);
+            new Solver().Solve(matrix, goalMatrix, 1000000);
+            Console.ReadKey();
+
+        }
+
+
+
+
+
+
+
+        private static int[,] getMatrix(String [] lines,int startIndex,int endIndex)
+        {
+            int[,] matrix = new int[3, 3];
+            int i = 0;
+            for(int lineNo = startIndex; lineNo <= endIndex; lineNo++)
+            {
+                String[] line = lines[lineNo].Replace("x", "-1").Split(' ');
+                matrix = getMatrix(matrix, line, i);
+                i++;
+            }
+            return matrix;
+        }
+
+        private static int[,] getMatrix(int[,] matrix,String[] line,int lineNo)
+        {
+            for(int i=0;i<line.Length;i++)
+            {
+                matrix[lineNo, i] = Convert.ToInt32(line[i]);
+            }
+            return matrix;
+        }
+       
+    }
+}
+
